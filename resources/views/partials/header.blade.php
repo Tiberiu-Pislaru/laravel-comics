86463a818de2dcedd7961e850d930800560b298c
Tiberiu-Pislaru <tiberiupislaru03@gmail.com>
2022-03-03 16:22:57 +0100

    configures various npm

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="{{asset('images/dc-logo.png')}}" alt="">
+        </div>
+        <nav>
+            <ul>
+    
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+                <li>ciao</li>
+            </ul>
+        </nav>
+
+    </div>
+    
+</header>
